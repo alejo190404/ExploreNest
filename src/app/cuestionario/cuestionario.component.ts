2808e00f1d9e23e44dca69a17919fee011bd93e9
mcardonar <93059205+mcardonar@users.noreply.github.com>
2024-04-26 15:28:32 -0500

    Cargando landing final

--- src/app/cuestionario/cuestionario.component.ts
+++ src/app/cuestionario/cuestionario.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./cuestionario.component.css']
 })
 export class CuestionarioComponent {
+  responses: any = {
+    nightLife: false,
+    historical: false,
+    nature:false,
+    beach:false,
 
+    camping:false,
+    eco:false,
+    hoteles:false,
+
+    alquiler:false,
+    publico:false,
+    privado:false,
+
+    museos:false,
+    urban:false,
+    deportes:false,
+    natureeco:false,
+
+  };
+
+  constructor() {
+    
+  }
+
+  Siguiente() {
+    
+  }
 }
